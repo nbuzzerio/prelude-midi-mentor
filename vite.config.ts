54a944791a513860cc241c0c0536008c05b0ca93
nbuzzerio <nick.buzzerio@hotmail.com>
2026-07-15 21:51:39 -0400

    ci: add static deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/prelude/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
